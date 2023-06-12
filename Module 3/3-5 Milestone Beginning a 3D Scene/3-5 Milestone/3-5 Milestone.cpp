90d700aab20fd61890f74bb1a5c6e0c6a9414229
Emily-Wood7 <95588376+Emily-Wood7@users.noreply.github.com>
2023-06-12 15:38:37 -0400

    Add files via upload

---
+++ Module 3/3-5 Milestone Beginning a 3D Scene/3-5 Milestone/3-5 Milestone.cpp
@@ -0,0 +1,364 @@
+﻿/*
+* Emily Wood
+* CS 330
+* 5/21/2023
+* 3-5 Milestone
+* meshes.h and meshes.cpp used from
+* Professor Battersby
+*/
+
+#include <iostream>         // cout, cerr
+#include <cstdlib>          // EXIT_FAILURE
+#include <GL/glew.h>        // GLEW library
+#include <GLFW/glfw3.h>     // GLFW library
+
+// GLM Math Header inclusions
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "meshes.h"
+
+using namespace std; // Standard namespace
+
+/*Shader program Macro*/
+#ifndef GLSL
+#define GLSL(Version, Source) "#version " #Version " core \n" #Source
+#endif
+
+// Unnamed namespace
+namespace
+{
+	const char* const WINDOW_TITLE = "Emily Wood 3-5 Milestone: Building a 3D Scene"; // Macro for window title
+
+	// Variables for window width and height
+	const int WINDOW_WIDTH = 800;
+	const int WINDOW_HEIGHT = 600;
+
+	// Stores the GL data relative to a given mesh
+	struct GLMesh
+	{
+		GLuint vao;         // Handle for the vertex array object
+		GLuint vbos[2];     // Handles for the vertex buffer objects
+		GLuint nIndices;    // Number of indices of the mesh
+	};
+
+	// Main GLFW window
+	GLFWwindow* gWindow = nullptr;
+	// Triangle mesh data
+	//GLMesh gMesh;
+	// Shader program
+	GLuint gProgramId;
+
+	//Shape Meshes from Professor Brian
+	Meshes meshes;
+}
+
+/* User-defined Function prototypes to:
+ * initialize the program, set the window size,
+ * redraw graphics on the window when resized,
+ * and render graphics on the screen
+ */
+bool UInitialize(int, char* [], GLFWwindow** window);
+void UResizeWindow(GLFWwindow* window, int width, int height);
+void UProcessInput(GLFWwindow* window);
+//void UCreateMesh(GLMesh &mesh);
+//void UDestroyMesh(GLMesh &mesh);
+void URender();
+bool UCreateShaderProgram(const char* vtxShaderSource, const char* fragShaderSource, GLuint& programId);
+void UDestroyShaderProgram(GLuint programId);
+
+
+/* Vertex Shader Source Code*/
+const GLchar* vertexShaderSource = GLSL(440,
+	layout(location = 0) in vec3 position; // Vertex data from Vertex Attrib Pointer 0
+layout(location = 1) in vec4 color;  // Color data from Vertex Attrib Pointer 1
+
+out vec4 vertexColor; // variable to transfer color data to the fragment shader
+
+//Global variables for the  transform matrices
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+	gl_Position = projection * view * model * vec4(position, 1.0f); // transforms vertices to clip coordinates
+	vertexColor = color; // references incoming color data
+}
+);
+
+
+/* Fragment Shader Source Code*/
+const GLchar* fragmentShaderSource = GLSL(440,
+	in vec4 vertexColor; // Variable to hold incoming color data from vertex shader
+
+out vec4 fragmentColor;
+uniform vec4 uObjectColor;
+
+void main()
+{
+	//fragmentColor = vec4(vertexColor);
+	fragmentColor = vec4(uObjectColor);
+}
+);
+
+
+int main(int argc, char* argv[])
+{
+	if (!UInitialize(argc, argv, &gWindow))
+		return EXIT_FAILURE;
+
+	// Create the mesh
+	//UCreateMesh(gMesh); // Calls the function to create the Vertex Buffer Object
+	meshes.CreateMeshes();
+
+	// Create the shader program
+	if (!UCreateShaderProgram(vertexShaderSource, fragmentShaderSource, gProgramId))
+		return EXIT_FAILURE;
+
+	// Sets the background color of the window to black (it will be implicitely used by glClear)
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(gWindow))
+	{
+		// input
+		// -----
+		UProcessInput(gWindow);
+
+		// Render this frame
+		URender();
+
+		glfwPollEvents();
+	}
+
+	// Release mesh data
+	//UDestroyMesh(gMesh);
+	meshes.DestroyMeshes();
+
+	// Release shader program
+	UDestroyShaderProgram(gProgramId);
+
+	exit(EXIT_SUCCESS); // Terminates the program successfully
+}
+
+
+// Initialize GLFW, GLEW, and create a window
+bool UInitialize(int argc, char* argv[], GLFWwindow** window)
+{
+	// GLFW: initialize and configure
+	// ------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// GLFW: window creation
+	// ---------------------
+	* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+	if (*window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+	glfwMakeContextCurrent(*window);
+	glfwSetFramebufferSizeCallback(*window, UResizeWindow);
+
+	// GLEW: initialize
+	// ----------------
+	// Note: if using GLEW version 1.13 or earlier
+	glewExperimental = GL_TRUE;
+	GLenum GlewInitResult = glewInit();
+
+	if (GLEW_OK != GlewInitResult)
+	{
+		std::cerr << glewGetErrorString(GlewInitResult) << std::endl;
+		return false;
+	}
+
+	// Displays GPU OpenGL version
+	cout << "INFO: OpenGL Version: " << glGetString(GL_VERSION) << endl;
+
+	return true;
+}
+
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void UProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void UResizeWindow(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+
+// Functioned called to render a frame
+void URender()
+{
+	glm::mat4 scale;
+	glm::mat4 rotation;
+	glm::mat4 translation;
+	glm::mat4 model;
+	glm::mat4 view;
+	glm::mat4 projection;
+	GLint modelLoc;
+	GLint viewLoc;
+	GLint projLoc;
+	GLint objectColorLoc;
+
+	// Enable z-depth
+	glEnable(GL_DEPTH_TEST);
+
+	// Clear the frame and z buffers
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Transforms the camera
+	view = glm::translate(glm::vec3(0.0f, -4.0f, -12.0f));
+
+	// Creates a orthographic projection
+	//projection = glm::ortho(-5.0f, 5.0f, -5.0f, 5.0f, 0.1f, 100.0f);
+	projection = glm::perspective(glm::radians(60.0f), (GLfloat)WINDOW_WIDTH / (GLfloat)WINDOW_HEIGHT, 0.1f, 100.0f);
+
+	// Set the shader to be used
+	glUseProgram(gProgramId);
+
+	// Retrieves and passes transform matrices to the Shader program
+	modelLoc = glGetUniformLocation(gProgramId, "model");
+	viewLoc = glGetUniformLocation(gProgramId, "view");
+	projLoc = glGetUniformLocation(gProgramId, "projection");
+	objectColorLoc = glGetUniformLocation(gProgramId, "uObjectColor");
+
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	// cylinder for coffee mug
+	// Activate the VBOs contained within the mesh's VAO
+	glBindVertexArray(meshes.gCylinderMesh.vao);
+
+	// 1. Scales the object
+	scale = glm::scale(glm::vec3(3.0f, 4.0f, 3.0f));
+	// 2. Rotate the object
+	rotation = glm::rotate(0.0f, glm::vec3(1.0, 1.0f, 1.0f));
+	// 3. Position the object
+	translation = glm::translate(glm::vec3(0.0f, 2.0f, 0.0f));
+	// Model matrix: transformations are applied right-to-left order
+	model = translation * rotation * scale;
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	glProgramUniform4f(gProgramId, objectColorLoc, 1.0f, 0.961f, 0.933f, 1.0f);
+
+	// Draws the triangles
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 36);		//bottom
+	glDrawArrays(GL_TRIANGLE_FAN, 36, 36);		//top
+	glDrawArrays(GL_TRIANGLE_STRIP, 72, 146);	//sides
+
+	// Deactivate the Vertex Array Object
+	glBindVertexArray(0);
+
+	// coffee mug handle
+	// Activate the VBOs contained within the mesh's VAO
+	glBindVertexArray(meshes.gTorusMesh.vao);
+
+	// 1. Scales the object
+	scale = glm::scale(glm::vec3(1.5f, 2.0f, 1.5f));
+	// 2. Rotate the object
+	rotation = glm::rotate(0.0f, glm::vec3(1.0, 1.0f, 1.0f));
+	// 3. Position the object
+	translation = glm::translate(glm::vec3(2.7f, 4.0f, 0.0f));
+	// Model matrix: transformations are applied right-to-left order
+	model = translation * rotation * scale;
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	glProgramUniform4f(gProgramId, objectColorLoc, 1.0f, 0.961f, 0.933f, 1.0f);
+
+	// Draws the triangles
+	glDrawArrays(GL_TRIANGLES, 0, meshes.gTorusMesh.nVertices);
+
+	// Deactivate the Vertex Array Object
+	glBindVertexArray(0);
+
+	// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+	glfwSwapBuffers(gWindow);    // Flips the the back buffer with the front buffer every frame.
+}
+
+
+// Implements the UCreateShaders function
+bool UCreateShaderProgram(const char* vtxShaderSource, const char* fragShaderSource, GLuint& programId)
+{
+	// Compilation and linkage error reporting
+	int success = 0;
+	char infoLog[512];
+
+	// Create a Shader program object.
+	programId = glCreateProgram();
+
+	// Create the vertex and fragment shader objects
+	GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Retrive the shader source
+	glShaderSource(vertexShaderId, 1, &vtxShaderSource, NULL);
+	glShaderSource(fragmentShaderId, 1, &fragShaderSource, NULL);
+
+	// Compile the vertex shader, and print compilation errors (if any)
+	glCompileShader(vertexShaderId); // compile the vertex shader
+	// check for shader compile errors
+	glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShaderId, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+		return false;
+	}
+
+	glCompileShader(fragmentShaderId); // compile the fragment shader
+	// check for shader compile errors
+	glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShaderId, sizeof(infoLog), NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+		return false;
+	}
+
+	// Attached compiled shaders to the shader program
+	glAttachShader(programId, vertexShaderId);
+	glAttachShader(programId, fragmentShaderId);
+
+	glLinkProgram(programId);   // links the shader program
+	// check for linking errors
+	glGetProgramiv(programId, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(programId, sizeof(infoLog), NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+
+		return false;
+	}
+
+	glUseProgram(programId);    // Uses the shader program
+
+	return true;
+}
+
+
+void UDestroyShaderProgram(GLuint programId)
+{
+	glDeleteProgram(programId);
+}
